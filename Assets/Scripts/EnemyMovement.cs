8e0c13e97a92d9f7eedd1821d7b3e4dbe0420439
Anta <j0siddy@gmail.com>
2026-01-17 12:43:12 -0600

    Add variable enemy speed modifiers

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,6 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
     public List<Vector3> patrolPoints;
     private float cellSize;
+    public float speed = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,7 +21,7 @@ public class EnemyMovement : MonoBehaviour
     {
         if (patrolPoints.Count > 0)
         {
-            transform.position = Vector3.MoveTowards(transform.position, (new Vector3(patrolPoints[0].x * cellSize, 0f, patrolPoints[0].z * cellSize)), Time.deltaTime * 2);
+            transform.position = Vector3.MoveTowards(transform.position, (new Vector3(patrolPoints[0].x * cellSize, 0f, patrolPoints[0].z * cellSize)), Time.deltaTime * speed);
             if (Vector3.Distance(transform.position, patrolPoints[0]) < 0.1f)
             {
                 patrolPoints.RemoveAt(0);
